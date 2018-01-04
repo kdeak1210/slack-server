fad00e2829b324daab41e3a4def4cd63c0df0797
kyle <kdeak1210@gmail.com>
2018-01-04 11:15:31 -0500

    Added bcrypt hash to new incoming passwords, renamed createUSer mutation to register, refactored for async/await

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,3 +1,5 @@
+import bcrypt from 'bcrypt';
+
 export default {
   Query: {
     getUser: (parent, { id }, { models }) => models.User.findOne({ where: { id } }),
@@ -5,6 +7,14 @@ export default {
   },
 
   Mutation: {
-    createUser: (parent, args, { models }) => models.User.create(args),
+    register: async (parent, { password, ...otherArgs }, { models }) => {
+      try {
+        const hashedPassword = await bcrypt.hash(password, 12); // 12 salt rounds
+        models.User.create({ password: hashedPassword, ...otherArgs });
+        return true;
+      } catch (err) {
+        return false;
+      }
+    },
   },
 };
