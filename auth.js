2e6b099e4efd650ebcf4e1661cc5336b34ca02ac
kyle <kdeak1210@gmail.com>
2018-01-05 18:51:09 -0500

    Added auth utility file (uses JWT). Added login mutation to user Schema
    TODO: login method for user resolver

---
+++ auth.js
@@ -0,0 +1,87 @@
+import jwt from 'jsonwebtoken';
+import _ from 'lodash';
+import bcrypt from 'bcrypt';
+
+export const createTokens = async (user, secret, secret2) => {
+  const createToken = jwt.sign(
+    {
+      user: _.pick(user, ['id', 'isAdmin']),
+    },
+    secret,
+    {
+      expiresIn: '1m',
+    },
+  );
+
+  const createRefreshToken = jwt.sign(
+    {
+      user: _.pick(user, 'id'),
+    },
+    secret2,
+    {
+      expiresIn: '7d',
+    },
+  );
+
+  return Promise.all([createToken, createRefreshToken]);
+};
+
+export const refreshTokens = async (token, refreshToken, models, SECRET) => {
+  let userId = -1;
+  try {
+    const { user: { id } } = jwt.decode(refreshToken);
+    userId = id;
+  } catch (err) {
+    return {};
+  }
+
+  if (!userId) {
+    return {};
+  }
+
+  const user = await models.User.findOne({ where: { id: userId }, raw: true });
+
+  if (!user) {
+    return {};
+  }
+
+  try {
+    jwt.verify(refreshToken, user.refreshSecret);
+  } catch (err) {
+    return {};
+  }
+
+  const [newToken, newRefreshToken] = await createTokens(user, SECRET, user.refreshSecret);
+  return {
+    token: newToken,
+    refreshToken: newRefreshToken,
+    user,
+  };
+};
+
+export const tryLogin = async (email, password, models, SECRET) => {
+  const user = await models.User.findOne({ where: { email }, raw: true });
+  if (!user) {
+    // User with the provided email was not found
+    return {
+      ok: false,
+      errors: [{ path: 'email', message: 'Wrong email' }],
+    };
+  }
+
+  const valid = await bcrypt.compare(password, user.password);
+  if (!valid) {
+    // bad password
+    return {
+      ok: false,
+      errors: [{ path: 'password', message: 'Wrong password' }],
+    };
+  }
+
+  const [token, refreshToken] = await createTokens(user, SECRET, user.refreshSecret);
+
+  return {
+    token,
+    refreshToken,
+  };
+};
