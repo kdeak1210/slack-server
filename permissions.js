221cb824c2a587ed532bbc14a3de321c0dbb036b
kyle <kdeak1210@gmail.com>
2018-01-11 01:46:32 -0500

    Added file permissions.js that has methods to add authorization checks to gql resolvers. I wrapped the createTeam resolver in the requiresAuth method which will check to see if a user is logged in before they are able to create a team. This works by first checking the gql context for a user / user.id

---
+++ permissions.js
@@ -0,0 +1,27 @@
+/** Allows for the creation of a 'stack' of permissions checks
+ * ex. requiresAdmin first checks requiresAuth
+ */
+
+const createResolver = (resolver) => {
+  const baseResolver = resolver;
+  baseResolver.createResolver = (childResolver) => {
+    const newResolver = async (parent, args, context, info) => {
+      await resolver(parent, args, context, info);
+      return childResolver(parent, args, context, info);
+    };
+    return createResolver(newResolver);
+  };
+  return baseResolver;
+};
+
+export const requiresAuth = createResolver((parent, args, { user }) => {
+  if (!user || !user.id) {
+    throw new Error('Not authenticated');
+  }
+});
+
+export const requiresAdmin = requiresAuth.createResolver((parent, args, context) => {
+  if (!context.user.isAdmin) {
+    throw new Error('Requires admin access');
+  }
+});
