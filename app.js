6a311dd0a5bcba5e849a488ebe0ef917e8ce5670
kyle <kdeak1210@gmail.com>
2018-01-11 01:16:34 -0500

    Added custom middleware to add a user to the req express object. The user is then passed into context through gqlExpress.
    The addUser middleware checks if there is a valid token, then tries to verify its authenticity with the token and secret. If its good, req.user is set to user and next() is called. If there is an error here, then the token is invalid (expired or bad secret), so we try to refresh the token. If this works, new response headers are sent back to the client 'x-token' and 'x-refresh-token'. Then req.user is set to user and next() called

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
 import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 import cors from 'cors';
+import jwt from 'jsonwebtoken';
 
 import models from './models';
+import { refreshTokens } from './auth';
 
 const SECRET = '123123123';
 const SECRET2 = 'abcabcabc';
@@ -24,22 +26,48 @@ const app = express();
 // CORS * lets all sites access the server...
 app.use(cors('*'));
 
+// custom middleware
+const addUser = async (req, res, next) => {
+  const token = req.headers['x-token'];
+  if (token) {
+    try {
+      // verify token hasn't expired, and it was signed with the SECRET
+      const { user } = jwt.verify(token, SECRET);
+      req.user = user;
+    } catch (err) {
+      // token invalid (expired or wrong secret or ...) -> attempt to refresh
+      const refreshToken = req.headers['x-refresh-token'];
+      const newTokens = await refreshTokens(token, refreshToken, models, SECRET, SECRET2);
+
+      if (newTokens.token && newTokens.refreshToken) {
+        // send the client back new tokens in headers
+        res.set('Access-Control-Expose-Headers', 'x-token', 'x-refresh-token');
+        res.set('x-token', newTokens.token);
+        res.set('x-refresh-token', newTokens.refreshToken);
+      }
+
+      req.user = newTokens.user;
+    }
+  }
+  next();
+};
+
+app.use(addUser);
+
 const graphqlEndpoint = '/graphql';
 
 app.use(
   graphqlEndpoint,
   bodyParser.json(),
-  graphqlExpress({
+  graphqlExpress(req => ({
     schema,
     context: {
       models,
-      user: {
-        id: 1, // pass in temp for team owner
-      },
+      user: req.user, // from the addUser middleware
       SECRET,
       SECRET2,
     },
-  }),
+  })),
 );
 
 app.use('/graphiql', graphiqlExpress({ endpointURL: graphqlEndpoint }));
