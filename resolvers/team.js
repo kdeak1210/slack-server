f6257d7fe26a528d87f984a01602803c10ab21f8
kyle <kdeak1210@gmail.com>
2018-01-17 12:43:19 -0500

    Using a sequelize transaction to ensure that a team is only created if its 'general' channel is also successfulyl created
    If something goes wrong in either DB hit, an error will be thrown and returned to the front ned

--- resolvers/team.js
+++ resolvers/team.js
@@ -37,14 +37,19 @@ export default {
         };
       }
     }),
+
     createTeam: requiresAuth.createResolver(async (parent, args, { models, user }) => {
       try {
-        const team = await models.Team.create({ ...args, owner: user.id });
-        // general channel create (await prevents race condition)
-        await models.Channel.create({ name: 'general', public: true, teamId: team.id });
+        // Use 'transaction', ensures team not created if error creating 'general' channel
+        const response = await models.sequelize.transaction(async () => {
+          const team = await models.Team.create({ ...args, owner: user.id });
+          await models.Channel.create({ name: 'general', public: true, teamId: team.id });
+          return team;
+        });
+
         return {
           ok: true,
-          team,
+          team: response,
         };
       } catch (err) {
         console.log(err);
