d2ed684f18d8c4c95aab57b430e75d1a258dde73
kyle <kdeak1210@gmail.com>
2018-01-28 14:37:20 -0500

    Fixed sequelize transaction
    Needed to specify which queries are part of the transaction, rather than assuming all of them are automatically added..

--- resolvers/team.js
+++ resolvers/team.js
@@ -63,10 +63,16 @@ export default {
     createTeam: requiresAuth.createResolver(async (parent, args, { models, user }) => {
       try {
         // Use 'transaction', ensures team not created if error creating 'general' channel
-        const response = await models.sequelize.transaction(async () => {
-          const team = await models.Team.create({ ...args });
-          await models.Channel.create({ name: 'general', public: true, teamId: team.id });
-          await models.Member.create({ teamId: team.id, userId: user.id, admin: true });
+        const response = await models.sequelize.transaction(async (transaction) => {
+          const team = await models.Team.create({ ...args }, { transaction });
+          await models.Channel.create(
+            { name: 'general', public: true, teamId: team.id },
+            { transaction },
+          );
+          await models.Member.create(
+            { teamId: team.id, userId: user.id, admin: true },
+            { transaction },
+          );
           return team;
         });
 
