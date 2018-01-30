7d6b2583af4c6af7a735f96183030855a981bbb9
kyle <kdeak1210@gmail.com>
2018-01-30 13:02:35 -0500

    Purged the directMessage schema/resolver
    Direct messages are now handled as channels, so this work is done in the channel resolver

--- resolvers/channel.js
+++ resolvers/channel.js
@@ -4,40 +4,65 @@ import { requiresAuth } from '../permissions';
 export default {
   Mutation: {
     getOrCreateChannel: requiresAuth.createResolver(async (parent, { teamId, members }, { models, user }) => {
-      members.push(user.id); // The members' ids' who they select, and the currentUser
+      const member = await models.Member.findOne(
+        { where: { teamId, userId: user.id } },
+        { raw: true },
+      );
+
+      if (!member) {
+        // They aren't a member of the team (checked teamId, userId)
+        throw new Error('Not Authorized');
+      }
+
+      const allMembers = [...members, user.id]; // all their id's
       // check if dm channel already exists with these members (Note: @> in psql means 'contains')
       const [data, result] = await models.sequelize.query(`
         select c.id 
         from channels as c, pcmembers pc 
         where pc.channel_id = c.id and c.dm = true and c.public = false and c.team_id = ${teamId}
         group by c.id 
-        having array_agg(pc.user_id) @> Array[${members.join(',')}] and count(pc.user_id) = ${members.length};
+        having array_agg(pc.user_id) @> Array[${allMembers.join(',')}] and count(pc.user_id) = ${allMembers.length};
       `, { raw: true });
+
       console.log(data, result);
 
       if (data.length) {
         // If we have data, grab the id from the first one and thats our channel
-        return data[0].id;
+        return data[0];
       }
+
+      const users = await models.User.findAll({
+        raw: true,
+        where: {
+          id: {
+            [models.sequelize.Op.in]: members,
+          },
+        },
+      });
+
+      // Make a channel name by joining all the members
+      const name = users.map(u => u.username).join(', ');
 
       // There's no data returned, so we need to create the channel
       const channelId = await models.sequelize.transaction(async (transaction) => {
         const channel = await models.Channel.create({
-          name: 'hello',
+          name,
           public: false,
           dm: true,
           teamId,
         }, { transaction });
-
           // private channel, create a TABLE to know who is invited to the team
           // Filter currentuser out,add back in (member regardless if explicitly added self)
         const cId = channel.dataValues.id;
-        const pcmembers = members.map(m => ({ userId: m, channelId: cId }));
+        const pcmembers = allMembers.map(m => ({ userId: m, channelId: cId }));
         await models.PCMember.bulkCreate(pcmembers, { transaction });
         return cId;
       });
 
-      return channelId;
+      return {
+        id: channelId,
+        name,
+      };
     }),
     createChannel: requiresAuth.createResolver(async (parent, args, { models, user }) => {
       try {
