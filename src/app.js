15be11a0105f793a1ebe933b0fedd2ad74a37423
kyle <kdeak1210@gmail.com>
2018-02-02 07:51:51 -0500

    Models index now returns an async function to connect to DB
    This removes the need for wait-for-it script - the web service depends on db service, and they will finish in the correct order. *NGINX CONFIG: Also proxying the graphql subscriptions endpoint /subscriptions to handle websockets protocol in nginx config file mysite.template

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ import { SubscriptionServer } from 'subscriptions-transport-ws';
 import { apolloUploadExpress } from 'apollo-upload-server';
 import DataLoader from 'dataloader';
 
-import models from './models';
+import getModels from './models';
 import { refreshTokens } from './auth';
 import { channelBatcher } from './batchFunctions';
 
@@ -33,93 +33,102 @@ const app = express();
 // CORS * lets all sites access the server...
 app.use(cors('*'));
 
-// custom middleware
-const addUser = async (req, res, next) => {
-  const token = req.headers['x-token'];
-  if (token) {
-    try {
-      // verify token hasn't expired, and it was signed with the SECRET
-      const { user } = jwt.verify(token, SECRET);
-      req.user = user;
-    } catch (err) {
-      // token invalid (expired or wrong secret or ...) -> attempt to refresh
-      const refreshToken = req.headers['x-refresh-token'];
-      const newTokens = await refreshTokens(token, refreshToken, models, SECRET, SECRET2);
-
-      if (newTokens.token && newTokens.refreshToken) {
-        // send the client back new tokens in headers
-        res.set('Access-Control-Expose-Headers', 'x-token', 'x-refresh-token');
-        res.set('x-token', newTokens.token);
-        res.set('x-refresh-token', newTokens.refreshToken);
-      }
-
-      req.user = newTokens.user;
-    }
-  }
-  next();
-};
-
-app.use(addUser);
-
 const graphqlEndpoint = '/graphql';
-
-app.use(
-  graphqlEndpoint,
-  bodyParser.json(),
-  apolloUploadExpress(),
-  graphqlExpress(req => ({
-    schema,
-    context: {
-      models,
-      user: req.user, // from the addUser middleware
-      SECRET,
-      SECRET2,
-      channelLoader: new DataLoader(ids => channelBatcher(ids, models, req.user)),
-      serverUrl: `${req.protocol}://${req.get('host')}`,
-    },
-  })),
-);
-
-app.use(
-  '/graphiql',
-  graphiqlExpress({
-    endpointURL: graphqlEndpoint,
-    subscriptionsEndpoint: 'ws://localhost:8080/subscriptions',
-  }),
-);
 
 // ex http://localhost:8080/files/B1ACDWhrz-ReactLogo.png
 app.use('/files', express.static('files'));
 
 const server = createServer(app);
 
-// sync({ force: true }) to drop the DB.
-models.sequelize.sync({ }).then(() => {
-  server.listen(8080, () => {
-    // eslint-disable-next-line no-new
-    new SubscriptionServer(
-      {
-        execute,
-        subscribe,
-        schema,
-        onConnect: async ({ token, refreshToken }, webSocket) => {
-          if (token && refreshToken) {
-            try {
-              const { user } = jwt.verify(token, SECRET);
-              return { models, user }; // models and user going into context
-            } catch (err) {
-              const newTokens = await refreshTokens(token, refreshToken, models, SECRET, SECRET2);
-              return { models, user: newTokens.user };
-            }
-          }
+getModels().then((models) => {
+  if (!models) {
+    console.log('Could not connect to database');
+    return;
+  }
 
-          return { models };
+  // custom middleware
+  const addUser = async (req, res, next) => {
+    const token = req.headers['x-token'];
+    if (token) {
+      try {
+      // verify token hasn't expired, and it was signed with the SECRET
+        const { user } = jwt.verify(token, SECRET);
+        req.user = user;
+      } catch (err) {
+      // token invalid (expired or wrong secret or ...) -> attempt to refresh
+        const refreshToken = req.headers['x-refresh-token'];
+        const newTokens = await refreshTokens(token, refreshToken, models, SECRET, SECRET2);
+
+        if (newTokens.token && newTokens.refreshToken) {
+        // send the client back new tokens in headers
+          res.set('Access-Control-Expose-Headers', 'x-token', 'x-refresh-token');
+          res.set('x-token', newTokens.token);
+          res.set('x-refresh-token', newTokens.refreshToken);
+        }
+
+        req.user = newTokens.user;
+      }
+    }
+    next();
+  };
+
+  app.use(addUser);
+
+  app.use(
+    graphqlEndpoint,
+    bodyParser.json(),
+    apolloUploadExpress(),
+    graphqlExpress(req => ({
+      schema,
+      context: {
+        models,
+        user: req.user, // from the addUser middleware
+        SECRET,
+        SECRET2,
+        channelLoader: new DataLoader(ids => channelBatcher(ids, models, req.user)),
+        serverUrl: `${req.protocol}://${req.get('host')}`,
+      },
+    })),
+  );
+
+  app.use(
+    '/graphiql',
+    graphiqlExpress({
+      endpointURL: graphqlEndpoint,
+      subscriptionsEndpoint: 'ws://localhost:8080/subscriptions',
+    }),
+  );
+
+
+  // sync({ force: true }) to drop the DB.
+  models.sequelize.sync({ }).then(() => {
+    server.listen(8080, () => {
+    // eslint-disable-next-line no-new
+      new SubscriptionServer(
+        {
+          execute,
+          subscribe,
+          schema,
+          onConnect: async ({ token, refreshToken }, webSocket) => {
+            if (token && refreshToken) {
+              try {
+                const { user } = jwt.verify(token, SECRET);
+                return { models, user }; // models and user going into context
+              } catch (err) {
+                const newTokens = await refreshTokens(token, refreshToken, models, SECRET, SECRET2);
+                return { models, user: newTokens.user };
+              }
+            }
+
+            return { models };
+          },
         },
-      },
-      {
-        server,
-        path: '/subscriptions',
-      },
-    );
+        {
+          server,
+          path: '/subscriptions',
+        },
+      );
+    });
   });
 });
+
--- src/models/index.js
+++ src/models/index.js
@@ -1,34 +1,56 @@
 import Sequelize from 'sequelize';
 
-// Init, (db_name, username, password)
-const sequelize = new Sequelize(process.env.TEST_DB || 'slack', 'postgres', 'postgres', {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  operatorsAliases: Sequelize.Op,
-  host: process.env.DB_HOST || 'localhost', // by default looks for localhost
-  define: {
-    underscored: true,
-  },
-});
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-const models = {
-  User: sequelize.import('./user'),
-  Channel: sequelize.import('./channel'),
-  Message: sequelize.import('./message'),
-  Team: sequelize.import('./team'),
-  Member: sequelize.import('./member'),
-  DirectMessage: sequelize.import('./directMessage'),
-  PCMember: sequelize.import('./pcmember'),
-};
+export default async () => {
+  let connected = false;
+  let maxReconnects = 20;
+  let sequelize;
 
-// Loop through all the models and associate them together
-Object.keys(models).forEach((modelName) => {
-  if ('associate' in models[modelName]) {
-    models[modelName].associate(models);
+  while (!connected && maxReconnects) {
+    try {
+      // Init, (db_name, username, password)
+      sequelize = new Sequelize(process.env.TEST_DB || 'slack', 'postgres', 'postgres', {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        operatorsAliases: Sequelize.Op,
+        host: process.env.DB_HOST || 'localhost', // by default looks for localhost
+        define: {
+          underscored: true,
+        },
+      });
+      connected = true;
+    } catch (err) {
+      console.log('Reconnecting in 5 seconds');
+      // eslint-disable-next-line no-await-in-loop
+      await sleep(5000);
+      maxReconnects -= 1;
+    }
   }
-});
 
-models.sequelize = sequelize;
-models.Sequelize = Sequelize;
+  if (!connected) {
+    return null;
+  }
 
-export default models;
+  const models = {
+    User: sequelize.import('./user'),
+    Channel: sequelize.import('./channel'),
+    Message: sequelize.import('./message'),
+    Team: sequelize.import('./team'),
+    Member: sequelize.import('./member'),
+    DirectMessage: sequelize.import('./directMessage'),
+    PCMember: sequelize.import('./pcmember'),
+  };
+
+  // Loop through all the models and associate them together
+  Object.keys(models).forEach((modelName) => {
+    if ('associate' in models[modelName]) {
+      models[modelName].associate(models);
+    }
+  });
+
+  models.sequelize = sequelize;
+  models.Sequelize = Sequelize;
+
+  return models;
+};
